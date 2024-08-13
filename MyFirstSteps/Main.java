b669e9969c3b552df842c491865d5a61fd6529e8
H4n-Z <nickhoelser@gmail.com>
2024-08-13 13:02:02 +0000

    fixed and deleted old

---
+++ MyFirstSteps/Main.java
@@ -0,0 +1,31 @@
+public class Main{
+public static void main(String[] args) {
+    int z1 = 5;
+    int z2 = 3;
+    float a1;
+
+    a1 = z1 ++;
+
+    System.out.println(a1);
+
+    a1 = z1 + z2;
+
+    System.out.println(a1);
+
+    a1 = z1 - z2;
+
+    System.out.println(a1);
+
+    a1 = z1 * z2;
+
+    System.out.println(a1);
+
+    a1 = z1 / z2;
+
+    System.out.println(a1);
+
+    a1 = z1 % z2;
+
+    System.out.println(a1);
+}
+}
